95dc0401d387dc548760de04fb8643bc641be6a9
ixlei <1099461262@qq.com>
2015-09-25 15:48:15 -0700

    uppdate visitor controller

--- resources/scripts/controller/app.controller.visitor.js
+++ resources/scripts/controller/app.controller.visitor.js
@@ -12,7 +12,7 @@ appCtrlVis.controller('regController', function($scope) {
 
     $scope.class = {
         username: '',
-        password: ''
+            password: ''
     };
 
     $scope.nameName = {
@@ -25,5 +25,51 @@ appCtrlVis.controller('regController', function($scope) {
         id: '身份证号'
     };
 
-    $scope.userType = 'investor';
-})
+    $scope.userType = '2';
+});
+
+appCtrlVis.controller('loginController', function($scope) {
+    $scope.user = {
+        logintype: '2',
+        feedback: ''
+    };
+
+});
+
+appCtrlVis.controller('circleController', function($scope) {
+    $scope.circle = {
+        dataSet: [10, 50, 25, 20, 80],
+        arc: {
+            innerWidth: 40,
+            outerWidth: 70
+        },
+        svg: {
+            translate: {
+                x: 20,
+                y: 20
+            },
+            width: 160,
+            height: 160,
+            text: {
+                text: '资本分布',
+                color: '#767576',
+                fontSize: '0.7em',
+                translate: {
+                    x: 50,
+                    y: 70
+                }
+            }
+        },
+        g: {
+            translate: {
+                x: 70,
+                y: 70
+            },
+            stroke: 'white',
+            strokeWidth: 1,
+            text: false,
+            animation: 60
+        },
+
+    };
+});

1b34cfbabafac086875408977dbfcc20380cf21a
ixlei <1099461262@qq.com>
2015-09-25 15:42:10 -0700

    update

--- resources/scripts/services/app.service.visitor.js
+++ resources/scripts/services/app.service.visitor.js
@@ -1,27 +1,56 @@
-var vService = angular.module('app.service.visitor', ['restangular']);
+var vService = angular.module('app.service.visitor', []);
 
-vService.factory('userCheckService', function(Restangular, $http) {
+vService.config(function($httpProvider) {
+    $httpProvider.defaults.withCredentials = true;
+});
+
+vService.factory('userService', function($http, $httpParamSerializer) {
 
     var userService = {
         echeck: function(email) {
             var d = {
                 data: email
             };
-            var res = $http.post('/customer/echeck', d);
+
+            var res = $http({
+                method: 'POST',
+                url: '/ngSpring/customer/echeck',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                data: $httpParamSerializer(d),
+                withCredentials: true
+            });
             return res;
         },
 
-        cecheck: function(username) {
+        userCheck: function(req) {
             var d = {
-                data: username
+                data: req.data
             };
 
-            var oneUser = Restangular.one('customer', 'cecheck');
-            return oneUser.post(d).then(function(msg) {
-                return msg;
-            }, function(err) {
-                return null;
-            })
+            return $http({
+                method: 'POST',
+                url: req.url,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                withCredentials: true,
+                data: $httpParamSerializer(d)
+            });
+        },
+
+        firstPost: function(post) {
+            return $http({
+                method: 'POST',
+                url: post.url,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                withCredentials: true,
+                data: $httpParamSerializer(post.data)
+
+            });
         }
     }
 

aa318a32d566ea7ab92dd0626b968162bcf5c16b
ixlei <1099461262@qq.com>
2015-09-28 10:10:47 -0700

    company

--- resources/scripts/controller/app.controller.company.js
+++ resources/scripts/controller/app.controller.company.js
@@ -1,9 +1,44 @@
-var comCtrl = angular.module('app.controller.company', []);
+var comCtrl = angular.module('app.controller.company', [
+    'app.service.company'
+]);
 
-comCtrl.controller('financeController', function($scope, data, $log) {
-	$scope.publish = data.data;
+comCtrl.controller('financeController', function($scope, data) {
+    $scope.publish = data.data;
 });
 
 comCtrl.controller('pubController', function($scope) {
-	
+
+});
+
+comCtrl.controller('raisedbondsController', function($scope, data) {
+    $scope.bond = data.data;
+});
+
+comCtrl.controller('corCtrl', function($scope, corService) {
+    corService.get().then(function(res) {
+        $scope.cor = res.data.cor;
+    }, function(err) {
+
+    });
+});
+
+comCtrl.controller('stockController', function($scope, corService, $routeParams) {
+    corService.getStock($routeParams.id).then(function(res) {
+        $scope.stock = res.data.stock;
+        console.log(res);
+    }, function(err) {
+
+    });
+});
+
+comCtrl.controller('debtController', function($scope, corService, $routeParams) {
+    corService.getDebt($routeParams.id).then(function(res) {
+        $scope.debt = res.data.debt;
+    }, function(err) {
+
+    });
+});
+
+comCtrl.controller('chatController', function($scope, data) {
+	console.log(data)
 });

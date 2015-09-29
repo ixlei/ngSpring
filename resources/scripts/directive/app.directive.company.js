19167f92115539259e0645ae4326a4c6422de3bc
ixlei <1099461262@qq.com>
2015-09-29 10:32:08 -0700

    update

--- resources/scripts/directive/app.directive.company.js
+++ resources/scripts/directive/app.directive.company.js
@@ -7,14 +7,112 @@ comDirective.directive('pubDirective', function(financeService, $log) {
         restrict: 'A',
         link: function(scope, iElement, iAttrs) {
             iElement.bind('click', function(e) {
-            	$log.log(scope.pub);
-                var data = scope.pub;
-                financeService.post(data).then(function(res) {
-                    alert(res.data.res);
+                var d = scope.pub;
+                financeService.post(d).then(function(resp) {
+                    alert(resp.data.res);
                 }, function(err) {
                     alert('error');
                 });
             });
         }
+    }
+});
+
+comDirective.directive('raisedbondDirective', function(raisedbondService) {
+    return {
+        restrict: 'A',
+        link: function(scope, iElement, iAttrs) {
+            iElement.bind('click', function(e) {
+                var d = scope.pub;
+                raisedbondService.post(d).then(function(resp) {
+                    alert(resp.data.res);
+                }, function(err) {
+                    alert('error');
+                });
+            });
+        }
+    };
+});
+
+comDirective.directive('scrollDirective', function($interval, $timeout) {
+    return {
+        restrict: 'A',
+        controller: 'scrollController',
+        link: function(scope, iElement, iAttrs) {
+            var children = iElement.children(),
+                firChild = angular.element(children[0]),
+                secChild = angular.element(children[1]);
+
+            iElement.css({
+                position: 'relative',
+                overflow: 'hidden',
+                height: scope.parent.height + 'px'
+            });
+
+            firChild.css({
+                position: 'absolute',
+                top: 0,
+                left: 0
+            });
+
+            secChild.css({
+                position: 'absolute',
+                top: scope.parent.scrollHeight() + 'px',
+                left: 0
+            });
+
+            var flag = 0,
+                distance = 0,
+                arrDistance = [],
+                timer;
+            for (var i = 0; i < 2; i++) {
+                arrDistance.push(0);
+            }
+
+            function scroll() {
+                timer = $interval(function() {
+                    distance -= scope.parent.distance;
+                    arrDistance[0] = distance;
+                    arrDistance[1] = scope.parent.scrollHeight() + distance;
+                    if ((scope.parent.scrollHeight() + distance) == 0) {
+                        distance = 0;
+                        flag += 1;
+                        $timeout(function() {
+                            firChild.css({
+                                top: arrDistance[flag % 2] + 'px'
+                            });
+                            secChild.css({
+                                top: arrDistance[(flag + 1) % 2] + 'px'
+                            });
+
+                        }, scope.speedUp);
+
+                        if (flag % 2 == 0) {
+                            flag = 0;
+                        } else {
+                            flag = 1;
+                        }
+                    }
+                    firChild.css({
+                        top: arrDistance[flag % 2] + 'px'
+                    });
+                    secChild.css({
+                        top: arrDistance[(flag + 1) % 2] + 'px'
+                    });
+                }, scope.speedUp);
+            };
+
+            scroll();
+
+            iElement.bind('mouseover', function($event) {
+                $interval.cancel(timer);
+            });
+
+            iElement.bind('mouseout', function($event) {
+                scroll();
+            });
+
+        }
+
     };
 });

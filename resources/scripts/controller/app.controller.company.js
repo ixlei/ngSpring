635fa1797a3fa03fbdea5461863df3277754044f
ixlei <1099461262@qq.com>
2015-10-11 16:57:24 -0700

    chat

--- resources/scripts/controller/app.controller.company.js
+++ resources/scripts/controller/app.controller.company.js
@@ -1,5 +1,6 @@
 var comCtrl = angular.module('app.controller.company', [
-    'app.service.company'
+    'app.service.company',
+    'ngCookies'
 ]);
 
 comCtrl.controller('financeController', function($scope, data) {
@@ -35,13 +36,56 @@ comCtrl.controller('debtController', function($scope, corService, $routeParams) 
     });
 });
 
-comCtrl.controller('chatController', function($scope, chatService) {
-    chatService.getFriend().then(function(res) {
-        $scope.data = res.data;
-    }, function(err) {
+comCtrl.controller('chatController', ['$scope', '$cookies', '$cookieStore',
+    'chatService',
+    function($scope, $cookies, $cookieStore, chatService) {
+        chatService.getFriend().then(function(res) {
+            $scope.data = res.data;
+            $cookieStore.put('uid', $scope.data.session);
+        }, function(err) {
 
-    });
-});
+        });
+
+        $scope.chat = {
+            showToList: 'hide',
+            chatList: [],
+            index: 0,
+            currentTo: '',
+            showClick: function() {
+                this.showToList =
+                    this.showToList == 'show' ? 'hide' : 'show';
+            },
+            chooseTo: function(e) {
+                e = event || window.event;
+                var id = e.target.dataset.from,
+                    nickname = e.target.dataset.nickname;
+                this.chatList.push({
+                    id: id,
+                    nickname: nickname
+                });
+                this.index = this.chatList.length - 1;
+            },
+            delTo: function(e) {
+                e = event || window.event;
+                var id = e.target.dataset.id,
+                    list = this.chatList;
+                for (var i in list) {
+                    if (list[i].id == id) {
+                        break;
+                    }
+                }
+                list.splice(i, 1);
+            },
+            switchTo: function(e) {
+                e = event || window.event;
+                this.currentTo = e.target.dataset.id;
+                this.index = e.target.dataset.index;
+                console.log(e.target.dataset.index)
+                console.log(this.index)
+            }
+        }
+    }
+]);
 
 comCtrl.controller('scrollController', function($scope) {
     $scope.parent = {
@@ -69,7 +113,7 @@ comCtrl.controller('ipublishController', function($scope, data) {
     });
 });
 
-comCtrl.controller('pubController', function($scope) {
+comCtrl.controller('pubController', ['$scope', function($scope) {
     $scope.toPub = {
         dragTips: true,
         fileRead: false,
@@ -80,6 +124,4 @@ comCtrl.controller('pubController', function($scope) {
         },
         result: []
     };
-
-
-});
+}]);
